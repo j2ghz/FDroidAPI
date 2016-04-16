6d421e1911cc2be197d413372a5ba425e1fd679f
Jozef Hollý <j2.00ghz@gmail.com>
2016-04-16 21:38:24 +0200

    Add basic functionality

---
+++ src/F-DroidAPI/Parser.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace FDroid
+{
+    using System.IO;
+    using System.Threading.Tasks;
+    using System.Xml.Serialization;
+
+    using FDroidAPI;
+
+    public static class Parser
+    {
+        private const string MainRepo = @"https://f-droid.org/repo/index.xml";
+
+        public static async Task<Repo> GetRepo(string url)
+        {
+            var xml = new XmlSerializer(typeof(Repo));
+            return (Repo)xml.Deserialize(await GetRepoStream(url));
+        }
+
+        public static async Task<Repo> GetRepo()
+        {
+            return await GetRepo(MainRepo);
+        }
+
+        private static async Task<Stream> GetRepoStream(string url)
+        {
+            var req = new System.Net.Http.HttpClient();
+            var res = await req.GetAsync(url);
+            return await res.Content.ReadAsStreamAsync();
+        }
+
+    }
+}

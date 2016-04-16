fd1daeeccaa554370961ba30da0d427937f4db12
Jozef Hollý <j2.00ghz@gmail.com>
2016-04-17 00:03:49 +0200

    move tests, change targets and dependencies

---
+++ test/F-DroidAPI.Tests/UnitTests.cs
@@ -0,0 +1,18 @@
+﻿namespace FDroidAPI.Test
+{
+    using System.Threading.Tasks;
+
+    using FDroid;
+
+    using Xunit;
+
+    public class UnitTests
+    {
+        [Fact]
+        public async Task GetRepo()
+        {
+            var repo = await Parser.GetRepo();
+            Assert.NotNull(repo);
+        }
+    }
+}
